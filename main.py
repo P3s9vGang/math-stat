af7c2c9c6b6369fa0b92f9dfa938cc8fc492ed8a
P3s9vGang <andrexa33455@gmail.com>
2024-04-24 02:03:28 +0300

    refactor: refactor names of functions

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from tkinter.filedialog import askopenfilename
-from emwa import emwa
-from memwa import memwa
-from xbar_rbar import xbar_rbar
-from xbar_sbar import xbar_sbar
-from imrx import imrx
-from cusum import cusum
+
+from cusum import *
+from imrx import *
+from emwa import *
+from xbar_rbar import *
+from xbar_sbar import *
+from memwa import *
+
+# TODO - function to choose the type of chart3
+# TODO - names of charts
 
 path_file = askopenfilename()
 
-print("1. emwa (len subgroup = 1) \n2. memwa\n3. xbar_rbar (len subgroup 2 to 8)\n4. xbar_sbar (len subgroup from 9)\n5. imrx (len subgroup = 1)\n6. cusum (len subgroup = 1)")
-choose_chart = int(input())
+# print("1. emwa (len subgroup = 1) \n2. memwa\n3. xbar_rbar (len subgroup 2 to 8)\n4. xbar_sbar (len subgroup from 9)\n5. imrx (len subgroup = 1)\n6. cusum (len subgroup = 1)")
 
-match choose_chart:
-    case 1:
-        emwa(path_file)
-    case 2:
-        memwa(path_file)
-    case 3:
-        xbar_rbar(path_file)
-    case 4:
-        xbar_sbar(path_file)
-    case 5:
-        imrx(path_file)
-    case 6:
-        cusum(path_file)
-    case _:
-        print("error")
+
+t = emwa()
+t.gen(path_file)
+print(t.allData())
+
+x = memwa()
+x.gen(path_file)
+print(x.allData())
+
+#
+# t = emwa()
+# t.gen(path_file)
+# print(allData())
